f7cd7a41ef50f85c8cab9a1746671336cb124f91
Didee <miss.sonagi@gmail.com>
2017-06-10 03:58:50 +0900

    Edit hello.py - p 72

--- hello.py
+++ hello.py
@@ -1,22 +1,64 @@
-from flask.ext.script import Manager
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from flask import Flask, request, render_template
-from datetime import datetime
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import Form
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, session, redirect, url_for, flash
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from secret import STRING
+import os
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/')
+app.config['SECRET_KEY'] = STRING
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like ou have changed your name!')
+        session['name'] = form.name.data
+        form.name.date = ''
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 @app.errorhandler(404)
